f670a859c5337850fd942f1dd359f4484253a68a
vysocan <vysocan76@gmail.com>
2021-03-20 11:36:48 +0100

    Zone thread cosmetics changes.

--- ohs_th_zone.h
+++ ohs_th_zone.h
@@ -43,9 +43,11 @@ static THD_FUNCTION(ZoneThread, arg) {
       pushToMqtt(typeGroup, i, functionState);
     }
   }
+
   // Delay to allow MQTT to proceed
   chThdSleepSeconds(1);
-  // Manage group MQTT publish
+
+  // Manage zone MQTT publish
   for (uint8_t i=0; i < ALARM_ZONES ; i++) {
     if (GET_CONF_ZONE_ENABLED(conf.zone[i]) && GET_CONF_ZONE_MQTT_PUB(conf.zone[i])) {
       pushToMqtt(typeZone, i, functionName);
@@ -100,32 +102,35 @@ static THD_FUNCTION(ZoneThread, arg) {
     DBG_ZONE("\r\n");
 #endif
 
-    for(uint8_t i = 0; i < ALARM_ZONES; i++) {
-      if (GET_CONF_ZONE_ENABLED(conf.zone[i])){
+    for(uint8_t zoneNum = 0; zoneNum < ALARM_ZONES; zoneNum++) {
+      if (GET_CONF_ZONE_ENABLED(conf.zone[zoneNum])){
         // Remote zone
-        if (i >= HW_ZONES) {
-          // Switch remote balanced zone from PIR back to OK after 2 seconds, as they don't send OK
-          if ((GET_CONF_ZONE_BALANCED(conf.zone[i])) && (zone[i].lastEvent == 'P') &&
-              ((zone[i].lastPIR + 2) < getTimeUnixSec())) {
-            zone[i].lastEvent = 'O';
-            zone[i].lastOK = getTimeUnixSec();    // update current timestamp
-          }
-          switch(zone[i].lastEvent) {
-            case 'O': val = ALARM_OK; break;
-            case 'P': val = ALARM_PIR; break;
-            default:  val = ALARM_TAMPER; break;
+        if (zoneNum >= HW_ZONES) {
+          // Zone is connected
+          if (conf.zoneAddress[zoneNum - HW_ZONES] != 0) {
+            // Switch remote balanced zone from PIR back to OK after 2 seconds, as they don't send OK
+            if ((GET_CONF_ZONE_BALANCED(conf.zone[zoneNum])) && (zone[zoneNum].lastEvent == 'P') &&
+                ((zone[zoneNum].lastPIR + 2) < getTimeUnixSec())) {
+              zone[zoneNum].lastEvent = 'O';
+              zone[zoneNum].lastOK = getTimeUnixSec();    // update current timestamp
+            }
+            switch(zone[zoneNum].lastEvent) {
+              case 'O': val = ALARM_OK; break;
+              case 'P': val = ALARM_PIR; break;
+              default:  val = ALARM_TAMPER; break;
+            }
           }
         } else { // Local HW
           // Digital 0, Analog 1
-          if (GET_CONF_ZONE_TYPE(conf.zone[i])){
-            val = adcSamples[i];
+          if (GET_CONF_ZONE_TYPE(conf.zone[zoneNum])){
+            val = adcSamples[zoneNum];
             // Force unbalanced for analog zones
-            if (!GET_CONF_ZONE_BALANCED(conf.zone[i])){
+            if (!GET_CONF_ZONE_BALANCED(conf.zone[zoneNum])){
               if (val < ALARM_UNBALANCED) val = ALARM_OK;
               else                        val = ALARM_PIR;
             }
           } else {
-            switch(i) {
+            switch(zoneNum) {
               case 10: if (palReadPad(GPIOE, GPIOE_DIN_BOX)) val = ALARM_PIR;
                        else                                  val = ALARM_OK;
               break;
@@ -135,23 +140,23 @@ static THD_FUNCTION(ZoneThread, arg) {
         }
 
         //    alarm as tamper                            is PIR                                        make it tamper
-        if ((GET_CONF_ZONE_PIR_AS_TMP(conf.zone[i])) && (val >= ALARM_PIR_LOW && val <= ALARM_PIR_HI)) val = ALARM_TAMPER;
+        if ((GET_CONF_ZONE_PIR_AS_TMP(conf.zone[zoneNum])) && (val >= ALARM_PIR_LOW && val <= ALARM_PIR_HI)) val = ALARM_TAMPER;
 
         // get current zone group
-        groupNum = GET_CONF_ZONE_GROUP(conf.zone[i]);
+        groupNum = GET_CONF_ZONE_GROUP(conf.zone[zoneNum]);
 
         // Decide zone state
         switch((uint16_t)(val)){
           case ALARM_OK_LOW ... ALARM_OK_HI:
             // All but remote balanced node, they will not send OK only PIR and Tamper
-            if (!((i >= HW_ZONES) && (GET_CONF_ZONE_BALANCED(conf.zone[i])))) {
-              zone[i].lastEvent = 'O';
-              zone[i].lastOK = getTimeUnixSec();    // update current timestamp
+            if (!((zoneNum >= HW_ZONES) && (GET_CONF_ZONE_BALANCED(conf.zone[zoneNum])))) {
+              zone[zoneNum].lastEvent = 'O';
+              zone[zoneNum].lastOK = getTimeUnixSec();    // update current timestamp
             }
             break;
           case ALARM_PIR_LOW ... ALARM_PIR_HI:
             //     zone not have alarm                 group delay is 0
-            if (!(GET_ZONE_ALARM(zone[i].setting)) && (group[groupNum].armDelay == 0)){
+            if (!(GET_ZONE_ALARM(zone[zoneNum].setting)) && (group[groupNum].armDelay == 0)){
               // if group not enabled log error to log.
               if (!(GET_CONF_GROUP_ENABLED(conf.group[groupNum].setting))) {
                 if (!(GET_GROUP_DISABLED_FLAG(group[groupNum].setting)) && (groupNum < ALARM_GROUPS)) {
@@ -160,24 +165,24 @@ static THD_FUNCTION(ZoneThread, arg) {
                 }
               } else {
                 // group armed
-                if (GET_GROUP_ARMED(group[groupNum].setting) && (zone[i].lastEvent == 'P')) {
+                if (GET_GROUP_ARMED(group[groupNum].setting) && (zone[zoneNum].lastEvent == 'P')) {
                   // Group is not armed home or is armed gome and also is flaged as home zone
                   if ((!GET_GROUP_ARMED_HOME(group[groupNum].setting)) ||
                       ((GET_GROUP_ARMED_HOME(group[groupNum].setting)) &&
-                       (GET_CONF_ZONE_ARM_HOME(conf.zone[i])))) {
+                       (GET_CONF_ZONE_ARM_HOME(conf.zone[zoneNum])))) {
                     alarmEvent_t *outMsgA = chPoolAlloc(&alarmEvent_pool);
                     if (outMsgA == NULL) {
-                      if (!(GET_ZONE_FULL_FIFO(zone[i].setting))) {
-                        tmpLog[0] = 'Z'; tmpLog[1] = 'P'; tmpLog[2] = i;  pushToLog(tmpLog, 3);
+                      if (!(GET_ZONE_FULL_FIFO(zone[zoneNum].setting))) {
+                        tmpLog[0] = 'Z'; tmpLog[1] = 'P'; tmpLog[2] = zoneNum;  pushToLog(tmpLog, 3);
                         pushToLogText("FA"); // Alarm queue is full
                       }
-                      SET_ZONE_FULL_FIFO(zone[i].setting); // Set On Alarm queue is full
+                      SET_ZONE_FULL_FIFO(zone[zoneNum].setting); // Set On Alarm queue is full
                       continue; // Continue if no free space.
                     }
-                    tmpLog[0] = 'Z'; tmpLog[1] = 'P'; tmpLog[2] = i;  pushToLog(tmpLog, 3);
-                    SET_ZONE_ALARM(zone[i].setting); // Set alarm bit On
-                    CLEAR_ZONE_FULL_FIFO(zone[i].setting); // Set Off Alarm queue is full
-                    outMsgA->zone = i; outMsgA->type = zone[i].lastEvent;
+                    tmpLog[0] = 'Z'; tmpLog[1] = 'P'; tmpLog[2] = zoneNum;  pushToLog(tmpLog, 3);
+                    SET_ZONE_ALARM(zone[zoneNum].setting); // Set alarm bit On
+                    CLEAR_ZONE_FULL_FIFO(zone[zoneNum].setting); // Set Off Alarm queue is full
+                    outMsgA->zone = zoneNum; outMsgA->type = zone[zoneNum].lastEvent;
                     msg = chMBPostTimeout(&alarmEvent_mb, (msg_t)outMsgA, TIME_IMMEDIATE);
                     if (msg != MSG_OK) pushToLogText("FA"); // Alarm queue is full
                   }
@@ -185,14 +190,14 @@ static THD_FUNCTION(ZoneThread, arg) {
               }
             }
             // All but remote balanced node, they will not send OK only PIR and Tamper
-            if (!((i >= HW_ZONES) && (GET_CONF_ZONE_BALANCED(conf.zone[i])))) {
-              zone[i].lastEvent = 'P';
-              zone[i].lastPIR = getTimeUnixSec();    // update current timestamp
+            if (!((zoneNum >= HW_ZONES) && (GET_CONF_ZONE_BALANCED(conf.zone[zoneNum])))) {
+              zone[zoneNum].lastEvent = 'P';
+              zone[zoneNum].lastPIR = getTimeUnixSec();    // update current timestamp
             }
             break;
           default: // Line is cut or short or tamper, no difference to alarm event
             //  zone not have alarm
-            if (!(GET_ZONE_ALARM(zone[i].setting))){
+            if (!(GET_ZONE_ALARM(zone[zoneNum].setting))){
               // if group not enabled log error to log.
               if (!(GET_CONF_GROUP_ENABLED(conf.group[groupNum].setting))) {
                 if (!(GET_GROUP_DISABLED_FLAG(group[groupNum].setting)) && (groupNum < ALARM_GROUPS)) {
@@ -200,44 +205,44 @@ static THD_FUNCTION(ZoneThread, arg) {
                   tmpLog[0] = 'G'; tmpLog[1] = 'F'; tmpLog[2] = groupNum;  pushToLog(tmpLog, 3);
                 }
               } else {
-                if (zone[i].lastEvent == 'T') {
+                if (zone[zoneNum].lastEvent == 'T') {
                   alarmEvent_t *outMsg = chPoolAlloc(&alarmEvent_pool);
                   if (outMsg == NULL) {
-                    if (!(GET_ZONE_FULL_FIFO(zone[i].setting))) {
-                      tmpLog[0] = 'Z'; tmpLog[1] = 'T'; tmpLog[2] = i;  pushToLog(tmpLog, 3);
+                    if (!(GET_ZONE_FULL_FIFO(zone[zoneNum].setting))) {
+                      tmpLog[0] = 'Z'; tmpLog[1] = 'T'; tmpLog[2] = zoneNum;  pushToLog(tmpLog, 3);
                       pushToLogText("FA"); // Alarm queue is full
                     }
-                    SET_ZONE_FULL_FIFO(zone[i].setting); // Set On Alarm queue is full
+                    SET_ZONE_FULL_FIFO(zone[zoneNum].setting); // Set On Alarm queue is full
                     continue; // Continue if no free space.
                   }
-                  tmpLog[0] = 'Z'; tmpLog[1] = 'T'; tmpLog[2] = i;  pushToLog(tmpLog, 3);
-                  SET_ZONE_ALARM(zone[i].setting); // Set alarm bit On
-                  CLEAR_ZONE_FULL_FIFO(zone[i].setting); // Set Off Alarm queue is full
-                  outMsg->zone = i; outMsg->type = zone[i].lastEvent;
+                  tmpLog[0] = 'Z'; tmpLog[1] = 'T'; tmpLog[2] = zoneNum;  pushToLog(tmpLog, 3);
+                  SET_ZONE_ALARM(zone[zoneNum].setting); // Set alarm bit On
+                  CLEAR_ZONE_FULL_FIFO(zone[zoneNum].setting); // Set Off Alarm queue is full
+                  outMsg->zone = zoneNum; outMsg->type = zone[zoneNum].lastEvent;
                   msg = chMBPostTimeout(&alarmEvent_mb, (msg_t)outMsg, TIME_IMMEDIATE);
                   if (msg != MSG_OK) pushToLogText("FA"); // Alarm queue is full
                 }
               }
             }
             // All but remote balanced node, they will not send OK only PIR and Tamper
-            if (!((i >= HW_ZONES) && (GET_CONF_ZONE_BALANCED(conf.zone[i])))) {
-              zone[i].lastEvent = 'T';
-              zone[i].lastPIR = getTimeUnixSec();    // update current timestamp
+            if (!((zoneNum >= HW_ZONES) && (GET_CONF_ZONE_BALANCED(conf.zone[zoneNum])))) {
+              zone[zoneNum].lastEvent = 'T';
+              zone[zoneNum].lastPIR = getTimeUnixSec();    // update current timestamp
             }
             break;
         }
-        if (zone[i].eventSent != zone[i].lastEvent) {
-          zone[i].eventSent = zone[i].lastEvent;
+        if (zone[zoneNum].eventSent != zone[zoneNum].lastEvent) {
+          zone[zoneNum].eventSent = zone[zoneNum].lastEvent;
           // Triggers
           outMsgTrig = chPoolAlloc(&trigger_pool);
           if (outMsgTrig != NULL) {
             outMsgTrig->type = 'Z';
             outMsgTrig->address = 0;
             outMsgTrig->function = ' ';
-            outMsgTrig->number = i;
+            outMsgTrig->number = zoneNum;
             // As defined in zoneState[], 0 = OK
-            if (zone[i].lastEvent == 'O') outMsgTrig->value = 0;
-            else if (zone[i].lastEvent == 'P') outMsgTrig->value = 1;
+            if (zone[zoneNum].lastEvent == 'O') outMsgTrig->value = 0;
+            else if (zone[zoneNum].lastEvent == 'P') outMsgTrig->value = 1;
             else outMsgTrig->value = 2;
             msg = chMBPostTimeout(&trigger_mb, (msg_t)outMsgTrig, TIME_IMMEDIATE);
             if (msg != MSG_OK) {
@@ -247,7 +252,7 @@ static THD_FUNCTION(ZoneThread, arg) {
             pushToLogText("FT"); // Trigger queue is full
           }
           // MQTT
-          if (GET_CONF_ZONE_MQTT_PUB(conf.zone[i])) pushToMqtt(typeZone, i, functionState);
+          if (GET_CONF_ZONE_MQTT_PUB(conf.zone[zoneNum])) pushToMqtt(typeZone, zoneNum, functionState);
         }
       } // zone enabled
     } // for each alarm zone
