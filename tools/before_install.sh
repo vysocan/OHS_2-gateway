fe9789adee7b88a939d3f56c0e8bfed9a7e9e471
Adam Baron <vysocan76@gmail.com>
2025-12-22 11:04:13 +0100

    build fix

--- tools/before_install.sh
+++ tools/before_install.sh
@@ -6,7 +6,7 @@ cd /tmp
 
 wget https://github.com/xpack-dev-tools/arm-none-eabi-gcc-xpack/releases/download/v11.3.1-1.1/xpack-arm-none-eabi-gcc-11.3.1-1.1-linux-x64.tar.gz
 tar xzf xpack-arm-none-eabi-gcc-11.3.1-1.1-linux-x64.tar.gz
-export PATH=/tmp/pack-arm-none-eabi-gcc-11.3.1-1.1/bin:$PATH
+export PATH=/tmp/xpack-arm-none-eabi-gcc-11.3.1-1.1/bin:$PATH
 arm-none-eabi-gcc --version
 
 cd -
